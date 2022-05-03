cb5dbe6a397b495c29908e62df06bba437f1de66
villaindot2 <dankokodundundof@gmail.com>
2022-05-03 14:32:02 +0600

    Second

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-          <p>Dana</p>
+          <p>Dana</p> <p>Second com</p>
         </a>
       </header>
     </div>
